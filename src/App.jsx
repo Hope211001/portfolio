72964ade427e46f3cd6a64544e3c4a3d30b1c084
Hope211001 <fanantenanarafaniritsoa@gmail.com>
2025-04-02 12:53:35 +0300

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import './app.css'
+import Header from "./components/header"
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <button className="btn btn-primary">Salut</button>
+    </div>
+  )
+}
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header() {
+
+  return (
+    <div>
+        <h1>Salut Fana</h1>
+    </div>
+  )
+}
+
+export default Header
